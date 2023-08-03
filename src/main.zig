ccdf5d34b06d92dc60b07629dfc4ac495531e2d8
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2023-08-03 23:32:15 +0200

    CLI matches other implementations. Add .gitignore

--- src/main.zig
+++ src/main.zig
@@ -17,49 +17,20 @@ pub fn main() !void {
     defer files.deinit();
     var programs = std.ArrayList([]const u8).init(alloc);
     defer programs.deinit();
-    while (argsit.next()) |arg| {
-        if (std.mem.eql(u8, "--file", arg)) {
-            const fname = argsit.next() orelse return;
-            try files.append(fname);
-        } else if (std.mem.eql(u8, "--prog", arg)) {
-            const prog = argsit.next() orelse return;
-            try programs.append(prog);
-        }
-    }
-    for (programs.items) |prog| {
-        const ast = try parser.parse(alloc, prog);
-        defer alloc.free(ast);
-        defer {
-            for (ast) |*node| {
-                switch (node.data) {
-                    .arguments => |args| alloc.free(args),
-                    else => {},
-                }
-            }
-        }
-        var info = try analyzer.analyze(ast, alloc);
-        defer info.deinit();
-        const init = std.crypto.random.int(u64);
-        var xorisho = std.rand.DefaultPrng.init(init);
-        var vm = VM.init(alloc, xorisho.random());
-        defer vm.deinit();
-        try emit.emit(ast, alloc, &vm);
+    const arg = argsit.next() orelse return;
+    var program: []const u8 = "";
 
-        var stdout = std.io.getStdOut();
-        var raw_output = stdout.writer();
-        var output = std.io.bufferedWriter(raw_output);
-        const stdin = std.io.getStdIn();
-        var input = stdin.reader();
-        const exit_code = (try vm.execute(&output, input)) orelse 0;
-
-        std.debug.print("\nExit code: {}\n", .{exit_code});
-    }
-    for (files.items) |file| {
-        var f = try std.fs.cwd().openFile(file, .{});
+    if (std.mem.eql(u8, "--file", arg) or std.mem.eql(u8, "-f", arg)) {
+        const fname = argsit.next() orelse return;
+        var f = try std.fs.cwd().openFile(fname, .{});
         defer f.close();
-        const code = try f.readToEndAlloc(alloc, std.math.maxInt(usize));
-        defer alloc.free(code);
-        const ast = try parser.parse(alloc, code);
+        program = try f.readToEndAlloc(alloc, std.math.maxInt(usize));
+    } else if (std.mem.eql(u8, "-e", arg)) {
+        program = argsit.next() orelse return;
+    }
+
+    {
+        const ast = try parser.parse(alloc, program);
         defer alloc.free(ast);
         defer {
             for (ast) |*node| {
@@ -83,10 +54,7 @@ pub fn main() !void {
         const stdin = std.io.getStdIn();
         var input = stdin.reader();
         const exit_code = (try vm.execute(&output, input)) orelse 0;
-        if (exit_code != 0) {
-            std.os.exit(exit_code);
-        }
 
-        // std.debug.print("\nExit code: {}\n", .{exit_code});
+        std.process.exit(exit_code);
     }
 }
